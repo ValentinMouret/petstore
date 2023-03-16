b23fb8c2e086855aaaae73a78493b41b2ad267f1
Valentin Mouret <valentin.mou@gmail.com>
2023-03-16 11:07:56 +0100

    Implement the  endpoint

--- README.md
+++ README.md
@@ -6,5 +6,13 @@ This is an implementation of the [Petstore API](https://github.com/OAI/OpenAPI-S
 ### Installation
 You need Python and [Poetry](https://python-poetry.com) installed, then run: `poetry install`.
 
+You will also need prism: `npm ci`
+
 ### Run
 `poetry run uvicorn petstore.server:app --reload`
+
+## Prism
+We use the Prism proxy to develop our API server.
+First, have the API server running in a terminal, and open a new one for the proxy.
+
+Have it running with: `npx prism proxy spec.yaml http://127.0.0.1:8000 --errors`
--- petstore/server.py
+++ petstore/server.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from dataclasses import dataclass
+from typing import List, Optional
+
+from fastapi import FastAPI, HTTPException
+
+
+@dataclass
+class Pet:
+    id: int
+    name: str
+    tag: Optional[str] = None
+
+
+# You will probably have a database here instead.
+pets = {
+    pet.id: pet
+    for pet in [
+        Pet(0, "cat", "lazy"),
+        Pet(1, "dog", "good"),
+        Pet(2, "llama", "unusual"),
+    ]
+}
 
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"message": "hello world!"}
+@app.get("/pets")
+def list_pets(limit: Optional[int] = None) -> List[Pet]:
+    _pets = list(pets.values())
+    return _pets if not limit else _pets[:limit]
